dc73c8d0b32277e4514e8cdce9a802371e1915c2
Ubuntu <ubuntu@ip-172-31-84-40.ec2.internal>
2019-08-21 10:07:00 +0000

    updated Json, adjusted html, js, and ccc accordingly

--- js/pollution.js
+++ js/pollution.js
@@ -1,113 +1,103 @@
 queue()
-    .defer(d3.json, "data/EU_Data_Sim.json")
+    .defer(d3.json, "data/New_EU_28.json")
     .await(makeGraphs);
 
-function makeGraphs(error, pollutionData) {
-    var ndx = crossfilter(pollutionData);
 
-    show_region_emission_chart(ndx);
+function makeGraphs(error, euData) { /*refering to line 4 index */
 
-    show_region_emission_pie(ndx);
 
-    show_region_selector(ndx);
+    var ndx = crossfilter(euData);
     
+   /* var total_deaths_percent = reduceAvg(ndx, "DeathsTotal");*/
+    
+    // charts
+    
+    show_eu_population(ndx);
+    
+    show_eu_emission_total(ndx);
+    
+   /* show_total_deaths(ndx, total_deaths_percent);*/
+
     show_country_selector(ndx);
 
     show_eu_barchart(ndx);
-    
+
     show_death_v_gdp_correlation(ndx);
 
-    show_average_death_rate(ndx);
-    
-  /*  show_emission_above_rate_pie(ndx); */
-    
+    show_country_emission_pie(ndx);
 
- /*   show_country_death_toll(ndx); */
 
     dc.renderAll();
 
 }
 
-function show_region_selector(ndx) {
-    dim = ndx.dimension(dc.pluck("Region"));
-    group = dim.group()
 
-    dc.selectMenu("#region-selector")
-        .dimension(dim)
-        .group(group);
+
+function show_eu_population(ndx) {
+	var populationGroup = ndx.groupAll().reduceSum(function(d) {
+		return d.Population;
+	});
+	var populationNum = dc.numberDisplay("#eu-population");
+
+	populationNum
+		.group(populationGroup)
+		.valueAccessor(function(d) {
+			return d;
+		});
 }
+
+function show_eu_emission_total(ndx) {
+	var emissionGroup = ndx.groupAll().reduceSum(function(d) {
+		return d.EmissionsC;
+	});
+	var totalEmissNum = dc.numberDisplay("#eu-total_emission");
+
+	totalEmissNum
+		.group(emissionGroup)
+		.valueAccessor(function(d) {
+			return d;
+		});
+}
+
+/*
+// ==== % population born abroad number display 
+function show_total_deaths(ndx, total_deaths_percent) {
+
+	var total_deaths = dc.numberDisplay("#total-deaths");
+
+	total_deaths
+		.formatNumber(d3.format(".0%")) // format number as percentage
+		.group(total_deaths) // group brought in from makeCharts()
+		.valueAccessor(function(d) {
+			return d.average / 100; // divide by 100 to allow % number format
+		});
+} */
+
+
 
 function show_country_selector(ndx) {
     dim = ndx.dimension(dc.pluck("Country"));
     group = dim.group()
-    
+
     dc.selectMenu("#country-selector")
         .dimension(dim)
         .group(group);
 }
 
-
-function show_region_emission_chart(ndx) {
-    var region_dim = ndx.dimension(dc.pluck("Region"));
-    var total_emissions = region_dim.group().reduceSum(dc.pluck('EmissionsR'));
-    
-    var regionColors = d3.scale.ordinal()
-        .domain(["Africa", "Americas", "Asia" ,"China", "EU-28", "Europe", "India", "Middle East", "USA"])
-        .range(["Yellow", "Green", "Pink" ,"Gold", "Brown", "Blue", "Orange", "Blue", "Red"]);
-
-
-    dc.barChart("#pollution-chart")
-        .width(600)
-        .height(400)
-        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
-        .dimension(region_dim)
-        .group(total_emissions)
-        .transitionDuration(500)
-        .x(d3.scale.ordinal())
-        .xUnits(dc.units.ordinal)
-        .colors(regionColors)
-        .colorAccessor(function(d) {
-            return d.key[1];
-        })
-        .xAxisLabel("Countries")
-        .yAxisLabel("Co2 Emission Level (billion tonnes)")
-        .yAxis().ticks(8);
-        
-
-}
-
-
-
-function show_region_emission_pie(ndx) {
-    var area_dim = ndx.dimension(dc.pluck('Region'));
-    var total_emissions_pie = area_dim.group().reduceSum(dc.pluck('EmissionsR'));
-
-    dc.pieChart("#region-pie-chart")
-        .height(500)
-        .radius(110)
-        .transitionDuration(1500)
-        .dimension(area_dim)
-        .group(total_emissions_pie)
-        .externalLabels(20)
-        .drawPaths(true)
-        .minAngleForLabel(0);
-        
-}
-
 function show_eu_barchart(ndx) {
-    var region_dim = ndx.dimension(dc.pluck("Country"));
-    var total_emissions = region_dim.group().reduceSum(dc.pluck('EmissionsC'));
-    
+    var country_dim = ndx.dimension(dc.pluck("Country"));
+    var total_emissions = country_dim.group().reduceSum(dc.pluck('EmissionsC'));
+
     var countryColors = d3.scale.ordinal()
-        .domain(["Austria", "Belgium", "Bulgaria" ,"Croatia", "Cyprus", "Czech Republic", "Denmark", "Estonia", "Finland", "France", "Germany", "Greece", "Hungary", "Ireland", "Italy", "Latvia" , "Lithuania","Luxembourg","Malta", "Netherlands","Poland","Portugal", "Romania","Slovakia", "Slovenia",  "Spain", "Sweden", "United Kingdom"])
-        .range(["Red", "Yellow", "Pink" ,"Grey", "Brown", "Purple", "Orange", "Coffee", "Coral", "Emerald", "Gold", "Black", "Lemon", "Green", "Blue", "Violet" , "Cyan", "Crimson","Jade", "Indigo","Lime", "Magenta", "Olive","Pear", "Peach",  "Plum", "Ruby", "Salmon"]);
+        .domain(["Austria", "Belgium", "Bulgaria", "Croatia", "Cyprus", "Czech Republic", "Denmark", "Estonia", "Finland", "France", "Germany", "Greece", "Hungary", "Ireland", "Italy", "Latvia", "Lithuania", "Luxembourg", "Malta", "Netherlands", "Poland", "Portugal", "Romania", "Slovakia", "Slovenia", "Spain", "Sweden", "United Kingdom"])
+        .range(["Red", "#15EE68", "red", "Grey", "Brown", "Purple", "Orange", "Coffee", "Coral", "Emerald", "Gold", "Black", "Lemon", "Green", "Blue", "Violet", "#4B15EE", "Crimson", "Jade", "Indigo", "Lime", "Magenta", "Olive", "Pear", "Peach", "Plum", "Ruby", "Salmon"]);
 
 
     dc.barChart("#eu-pollution-chart")
-        .width(900)
-        .height(850)
-        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
-        .dimension(region_dim)
+        .width(500)
+        .height(550)
+        .margins({ top: 20, right: 40, bottom: 40, left: 40 })
+        .dimension(country_dim)
         .group(total_emissions)
         .transitionDuration(500)
         .x(d3.scale.ordinal())
@@ -120,35 +110,31 @@ function show_eu_barchart(ndx) {
         .yAxisLabel("Co2 Emission Level (million tonnes)")
         .yAxis().ticks(7);
 
+
 }
 
 
-
-
-/* scatter plot ideal for correlation between 2 different data items
-e.g. death v gdp */
-
-
-
 function show_death_v_gdp_correlation(ndx) {
-    
+
     var nationColors = d3.scale.ordinal()
-        .domain(["Austria", "Belgium", "Bulgaria" ,"Croatia", "Cyprus", "Czech Republic", "Denmark", "Estonia", "Finland", "France", "Germany", "Greece", "Hungary", "Ireland", "Italy", "Latvia" , "Lithuania","Luxembourg","Malta", "Netherlands","Poland","Portugal", "Romania","Slovakia", "Slovenia",  "Spain", "Sweden", "United Kingdom"])
-        .range(["Red", "Yellow", "Pink" ,"Grey", "Brown", "Purple", "Orange", "Coffee", "Coral", "Emerald", "Gold", "Black", "Lemon", "Green", "Blue", "Violet" , "Cyan", "Crimson","Jade", "Indigo","Lime", "Magenta", "Olive","Pear", "Peach",  "Plum", "Ruby", "Salmon"]);
-    
+        .domain(["Austria", "Belgium", "Bulgaria", "Croatia", "Cyprus", "Czech Republic", "Denmark", "Estonia", "Finland", "France", "Germany", "Greece", "Hungary", "Ireland", "Italy", "Latvia", "Lithuania", "Luxembourg", "Malta", "Netherlands", "Poland", "Portugal", "Romania", "Slovakia", "Slovenia", "Spain", "Sweden", "United Kingdom"])
+        .range(["Red", "Yellow", "Pink", "Grey", "Brown", "Purple", "Orange", "Coffee", "Coral", "Emerald", "Gold", "Black", "Lemon", "Green", "Blue", "Violet", "Cyan", "Crimson", "Jade", "Indigo", "Lime", "Magenta", "Olive", "Pear", "Peach", "Plum", "Ruby", "Salmon"]);
+
     var gdpDim = ndx.dimension(dc.pluck("GDP")) /* on x axis*/
-    var deathCoDim = ndx.dimension(function (d) {
-        return [d.GDP, d.Deaths, d.Country] /*first part on x axis*/
+    var deathCoDim = ndx.dimension(function(d) {
+        return [d.GDP, d.DeathsPer, d.Country] /*first part on x axis*/
     });
     var gdpAndDeathGroup = deathCoDim.group();
-    
+
     var minGdpDeathRate = gdpDim.bottom(1)[0].GDP;
     var maxGdpDeathRate = gdpDim.top(1)[0].GDP;
-    
-    
+
+
     dc.scatterPlot("#death-v-gdp-plot")
-        .width(600)
+        .width(650)
         .height(400)
+       /* height(window.innerHeight - 300)
+        .width(window.innerWidth - 65 % window)*/
         .x(d3.scale.linear().domain([minGdpDeathRate, maxGdpDeathRate])) /* as gdp is numerical i.e. 10 is more than 9 */
         .brushOn(false)
         .symbolSize(6)
@@ -166,190 +152,35 @@ function show_death_v_gdp_correlation(ndx) {
         .group(gdpAndDeathGroup)
         .margins({ top: 10, right: 50, bottom: 75, left: 75 })
         .xAxis().ticks(5);
-    
-    
-}
 
-function show_average_death_rate(ndx) {
-    var dim = ndx.dimension(dc.pluck("Country"));
-
-    function add_item(p, v) {
-        p.count++;
-        p.total += v.deaths
-        p.average = p.total / p.count;
-        return p;
-    }
-
-    function remove_item(p, v) {
-        p.count--;
-        if (p.count == 0) {
-            p.total = 0;
-            p.average = 0;
-        }
-        else {
-            p.total -= v.deaths;
-            p.average = p.total / p.count;
-        }
-        return p;
-    }
-
-    function intialise() {
-        return { count: 0, total: 0, average: 0 };
-    }
-
-
-    var averageDeathRate = dim.group().reduce(add_item, remove_item, intialise);
-
-    dc.barChart("#average_death_rate")
-        .width(600)
-        .height(400)
-        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
-        .dimension(dim)
-        .group(averageDeathRate)
-        .valueAccessor(function(d){
-            return d.value.average;
-        })
-        .transitionDuration(500)
-        .x(d3.scale.ordinal())
-        .xUnits(dc.units.ordinal)
-        .xAxisLabel("Countries")
-        .yAxisLabel("Death")
-        .yAxis().ticks(8);
-}
-
-/* function show_emission_above_rate_pie(ndx) {
-    var dim = ndx.dimension(dc.pluck("Country"));
-    var emiss_dimension = ndx.dimension(function (d) {
-            if (d.emissionsC > 300)
-                return "300 and above";
-            else
-                return "Below 300"
-        });
-        var emiss_group = size_dimension.group();
-        
-        dc.pieChart('#above-pie-chart')
-            .height(330)
-            .radius(90)
-            .dimension(emiss_dimension)
-            .group(emiss_group);
-        dc.renderAll();
-    }
-*/
-
-/*
-
-
-function show_country_death_as_percentage_of_pop(ndx) {
-    
-}
-
-function show_country_death_toll(ndx) {
-    var timeDim = ndx.dimension(dc.pluck("Deaths"));
-    var countryDim = ndx.dimension(function(d) {
-        return [d.Country, d.Deaths, d.Year];
-    });
-    var deathGroup = countryDim.group();
-
-    var minTime = timeDim.bottom(1)[0].Deaths;
-    var maxTime = timeDim.top(1)[0].Deaths;
-
-    dc.scatterPlot("#death-plot")
-        .width(550)
-        .height(400)
-        .x(d3.scale.linear().domain([minTime, maxTime]))
-        .brushOn(false)
-        .symbolSize(6)
-        .clipPadding(10)
-        .xAxisLabel("Deaths (rounded off to highest decimal point)")
-        .title(function(d) {
-            return "this is" + d.key[0];
-        })
-        .dimension(countryDim)
-        .group(deathGroup)
-        .margins({ top: 10, right: 50, bottom: 75, left: 75 })
-        .xAxis().ticks(3);
 
 }
 
-*/
 
+function show_country_emission_pie(ndx) {
+    var area_dim = ndx.dimension(dc.pluck('Country'));
+    var total_emissions_pie = area_dim.group().reduceSum(dc.pluck('EmissionsC'));
 
-
-
-
-/*
-    
-function makeGraphs(error, euData) { 
-    var ndx = crossfilter(euData);
-    
-    pollutionData.forEach(function(d) {
-        d.Emissions = parseInt(d.Emissions);
-        d.Year = parseInt(d.Year);
-    })
-    
-    //show_country_pollution(ndx);
-    
-    show_region_co2_emissions(ndx);
-    
-    dc.renderAll();
-    
-}*/
-
-
-
-/*function show_country_pollution(ndx) {
-    var dim = ndx.dimension(dc.pluck("Entity"));
-    var group = dim.group();
-    
-    dc.barChart("pollution-graph")
-        .width(400)
-        .height(300)
-        .margins({top: 10, right: 50, bottom: 30, left: 50})
-        .dimension(dim)
-        .group(group)
-        .transitionDuration(500)
-        .x(d3.scale.ordinal())
-        .xUnits(dc.units.ordinal)
-        .xAxisLabel("Countries")
-        .yAxis().ticks(20); 
-}*/
-
-
-/*function show_correlation_between_rating_and_viewership(ndx) {
-
-    var IMDBViewDim = ndx.dimension(function (d) {
-        return [d.viewers, d.rating, d.episode, d.season];
-    });
-
-
-    var IMDBViewGroup = IMDBViewDim.group();
-
-    var seasonColors = d3.scale.ordinal()
-        .domain(["1", "2", "3", "4", "5", "6", "7", "8"])
-        .range(['#6E403A ', '#175ac6', '#E57687', '#DACC3E', '#7FB7BE', '#545a2c', '#020202', '#705D56']);
-
-    dc.scatterPlot("#corrIMDBandViews")
-        .dimension(IMDBViewDim)
-        .group(IMDBViewGroup)
-        .width(700)
-        .height(400)
-        .colorAccessor(function (d) {
-            return d.key[3];
-        })
-
-        .colors(seasonColors)
-        .y(d3.scale.linear()
-            .domain([0, 10]))
-
-        .x(d3.scale.linear()
-            .domain([1, 14]))
-        .brushOn(false)
-        .symbolSize(8)
-        .xAxisLabel("Viewers (in million)")
-        .yAxisLabel("IMDB Rating")
-        .title(function (d) {
-            return d.key[2] + " was rated a " + d.key[1] + " and had a viewership of " + d.key[0] + " million.";
-        })
-        .clipPadding(5);
-
-} */
+    dc.pieChart("#country-pie-chart")
+        .width(450)
+        .height(500)
+        .innerRadius(50)
+        .externalRadiusPadding(45)
+        .transitionDuration(1500)
+        .dimension(area_dim)
+        .group(total_emissions_pie)
+        .externalLabels(20)
+        .drawPaths(true)
+        .minAngleForLabel(0)
+        .cap(5)
+        .legend(
+      dc
+        .legend()
+        .x(395)
+        .y(20)
+        .horizontal(false)
+        .itemHeight(5)
+        .gap(5)
+    );
+       /* .legend(dc.legend().x(270).y(0).itemHeight(8).gap(5));*/
+}
